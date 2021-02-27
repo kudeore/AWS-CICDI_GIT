e5aa7a91be86044ca2551aa44f9f7cf93587b32b
kudeore <76858641+kudeore@users.noreply.github.com>
2021-02-27 12:50:49 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import numpy as np
+import os
+
+
+from flask import Flask, render_template
+import pickle
+import requests
+import json
+from flask import Response
+from io import StringIO
+
+
+application = Flask(__name__,template_folder='template')
+@application.route('/')
+def home():
+    return render_template('index.html')
+    
+
+    
+
+
+
+if __name__ == '__main__':
+    application.run(debug=True,use_reloader=False)
+
+
+
+
+
+
+
